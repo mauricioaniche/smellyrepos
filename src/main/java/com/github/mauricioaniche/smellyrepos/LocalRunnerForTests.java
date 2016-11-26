8094c91e80ce599c00bf68126660e0d9a2f6c4df
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-26 12:41:18 +0100

    production runner

--- src/main/java/com/github/mauricioaniche/smellyrepos/LocalRunnerForTests.java
+++ src/main/java/com/github/mauricioaniche/smellyrepos/LocalRunnerForTests.java
@@ -3,22 +3,24 @@ package com.github.mauricioaniche.smellyrepos;
 import java.io.IOException;
 import java.io.PrintStream;
 
-public class Runner {
+public class LocalRunnerForTests {
 
 	public static void main(String[] args) throws IOException {
 		
-		if(args.length < 2) {
-			System.out.println("You should pass <project path> <repo regex> <output file>");
-			System.exit(-1);
-		}
-
-		String projectPath = args[0];
-		String regex = args[1];
+//		if(args.length < 2) {
+//			System.out.println("You should pass <project path> <repo regex> <output file>");
+//			System.exit(-1);
+//		}
+		
+//		String projectPath = args[0];
+//		String regex = args[1];
+		
+		String projectPath = "/Users/mauricioaniche/workspace/gnarus";
+		String regex = "(.*)dao";
 		
 		PrintStream ps = System.out;
 		if(args!=null && args.length == 3) ps = new PrintStream(args[2]);
 		
 		new SmellyRepoDetector(ps, regex).execute(projectPath);
-		ps.close();
 	}
 }
